e45971880df28408a14f4b05328b9f172eb7d2c3
MuhammadRazaJawa <muhammadrazajawa@gmail.com>
2014-11-12 23:56:19 -0800

    After Conflic One commit test
    Challo theek hai

---
+++ src/main/java/org/orangedevelopers/consultancy/solution/service/AstapakhoraService.java
@@ -0,0 +1,11 @@
+package org.orangedevelopers.consultancy.solution.service;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AstapakhoraService<E> {
+	
+	public List<E> Lhun;
+
+}
