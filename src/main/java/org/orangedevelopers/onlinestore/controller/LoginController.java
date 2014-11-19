ed5cd55c824ad3358c550460ccf90539f5748a03
vickshah129 <vickyshah129@gmail.com>
2014-11-19 07:26:36 -0800

    Removed Non sense Data Members of this class. Dnt do it again please.

--- src/main/java/org/orangedevelopers/onlinestore/controller/LoginController.java
+++ src/main/java/org/orangedevelopers/onlinestore/controller/LoginController.java
@@ -10,11 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	
-	public String youJustShutTheFuckUpWaqas;
-	public String Bhenchod;
-	public String LunMoa;
-	
+		
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String loginUser(){
